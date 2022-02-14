dad12ed778d8e8dc42ff16ce7c6d74f8db8cab99
mikik <mikik@DESKTOP-U8QPC8N>
2022-02-14 20:45:03 +0100

    cambio y borrado

--- src/demo1/demo.java
+++ src/demo1/demo.java
@@ -2,8 +2,6 @@ package demo1;
 
 public class demo {
 	public static void main (String[] args) {
-		System.out.println("Hola");
-		System.out.println("Linea 1");
-		System.out.println("linea2");
+		
 	}
 }
