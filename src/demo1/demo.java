b850c3604c7384420559e69ae5c484eccd6b484b
mikik <mikik@DESKTOP-U8QPC8N>
2022-02-14 20:48:58 +0100

    cambio 2 desde mars en rama cambio2

--- src/demo1/demo.java
+++ src/demo1/demo.java
@@ -2,6 +2,6 @@ package demo1;
 
 public class demo {
 	public static void main (String[] args) {
-		
+		//Cambio desde mars
 	}
 }
---
+++ src/demo1/demo2.java
@@ -0,0 +1,9 @@
+package demo1;
+
+public class demo2 {
+	public static void main (String[] args) {
+		System.out.println("Hola");
+		System.out.println("Linea 1");
+		System.out.println("linea2");
+	}
+}
